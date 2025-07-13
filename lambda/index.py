fef00a561b545e34def39771eed39710b604df6e
gelson <gelson_m@hotmail.com>
2025-07-13 15:32:23 +0100

    making few adjustments and complete lambda function index.py

--- lambda/index.py
+++ lambda/index.py
@@ -5,3 +5,11 @@ import base64
 def handler(event, context):
     s3 = boto3.client('s3', endpoint_url="http://localhost:4566")
     bucket_name = os.environ['BUCKET_NAME']
+
+    image_data = base64.b64decode(event['body'])
+    s3.put_object(Bucket=bucket_name, Key='uploaded_image.jpg', Body=image_data)
+
+    return {
+        'statusCode': 200,
+        'body': 'Image uploaded successfully.'
+    }
